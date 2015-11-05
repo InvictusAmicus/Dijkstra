6e3666cac50f2ca6fe7fd235ed010aa5459abd6f
CiaranClerkin <K00191500@student.lit.ie>
2015-11-05 16:49:04 +0000

    readfile

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -14,27 +14,33 @@ public class ReadFile{
 	int Nodex, Nodey, NodeWeight;
 	//System.out.print("test");
         //Scanner scan = new Scanner(new File("Points.txt"));
+        
         String workingDir = System.getProperty("user.dir");
         System.out.println(workingDir);
-        Scanner scan = new Scanner(new File(workingDir+"Points.txt"));
+        Scanner scan = new Scanner(new File(workingDir+"\\src\\ReadFile\\point.txt"));
         GraphType = scan.next().charAt(0);
         Connections = scan.nextInt();
         int[] nodex = new int[Connections];
         int[] nodey = new int[Connections];
         int[] nodeweight = new int[Connections];
+        
         while(scan.hasNextInt()){
+            
             Nodex = scan.nextInt();
             nodex[i] = Nodex;
+         
             Nodey = scan.nextInt();
-            nodey[i] = Nodex;
+            nodey[i] = Nodey;
+           
             NodeWeight = scan.nextInt();
-            nodeweight[i] = Nodex;
+            nodeweight[i] = NodeWeight;
             i++;
         }
+    
         System.out.println(GraphType);
         System.out.println(Connections);
         for(int j=0; j<Connections;j++){
         System.out.println(nodex[j]+" "+nodey[j]+" "+nodeweight[j]);
-        }
+        }  
     }
 }
