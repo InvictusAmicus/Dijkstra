e01eec40733af439dd20cebef194d362a1766eac
InvictusAmicus <theinvictuscomplex@gmail.com>
2015-11-12 14:58:40 +0000

    Deal with it

--- src/DeclanCiaranDijkstra.java
+++ src/DeclanCiaranDijkstra.java
@@ -2,31 +2,53 @@ import java.util.*;
 import java.io.*;
 import java.awt.*;
 
+
 public class DeclanCiaranDijkstra {
+
+
     //distance between paths
     static int distance[];
     //Tree Structure Path
     static int TSP[][];
     static char GraphType;
     static int Connections;
+
+
+
     public static void main(String[]args)throws FileNotFoundException{
     	readFile();
         for(int j=0; j<Connections;j++){
         System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
-        } 
+        }
+
+
+
+
+         LinkedList<Node> list = new LinkedList<Node>();
+
+		        Node[] nodeArray = new Node[Connections];
+		        for(int k = 0; k < Connections; k++)
+		        {
+					nodeArray[k] = new Node(TSP[k][0],TSP[k][1],distance[k]);
+
+					list.add(nodeArray[k]);
+				}
+
+				System.out.println(list);
+
     }
-    
-    
+
+
     static void readFile()throws FileNotFoundException{
         int i =0;
 	int NodeX, NodeY, NodeWeight;
 	//System.out.print("test");
         //Scanner scan = new Scanner(new File("Points.txt"));
-        
+
         String workingDir = System.getProperty("user.dir");
         System.out.println(workingDir);
         //Scanner scan = new Scanner(new File(workingDir+"\\src\\ReadFile\\DensePoints.txt"));
-        Scanner scan = new Scanner(new File("point.txt"));
+        Scanner scan = new Scanner(new File("DensePoints.txt"));
         GraphType = scan.next().charAt(0);
         Connections = scan.nextInt();
         //int[] nodex = new int[Connections];
@@ -35,25 +57,28 @@ public class DeclanCiaranDijkstra {
         TSP = new int[Connections][2];
         distance = new int[Connections];
         while(scan.hasNextInt()){
-            
+
             NodeX = scan.nextInt();
             //nodex[i] = Nodex;
             NodeY = scan.nextInt();
             //nodey[i] = Nodey;
-           
+
             TSP[i][0] = NodeX;
             TSP[i][1] = NodeY;
             NodeWeight = scan.nextInt();
             distance[i] = NodeWeight;
             i++;
         }
-    
+
         System.out.println(GraphType);
         System.out.println(Connections);
         //for(int j=0; j<Connections;j++){
         //System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
-        //}  
-        
-    }
-    
+        //return new Node();
+
+        // -----------------------It#s algorithm time
+
+
+	}
+
 }
---
+++ src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+
+		private int vertex;
+		private int distance;
+		private int vertex2;
+
+		Node(int a,int b,int c) {
+			vertex = a;
+			vertex2 = b;
+			distance = c;
+		}
+	}
