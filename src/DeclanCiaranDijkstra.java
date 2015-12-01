f64333772800b7d1ef30a93a7b053c70c2419c50
CiaranClerkin <K00191500@student.lit.ie>
2015-12-01 12:33:12 +0000

    updated code

--- src/DeclanCiaranDijkstra.java
+++ src/DeclanCiaranDijkstra.java
@@ -3,9 +3,11 @@ import java.io.*;
 import java.awt.*;
 
 
+
 public class DeclanCiaranDijkstra {
 
-
+    static double[] distTo;
+    static Node[] edgeTo;
     //distance between paths
     static int distance[];
     //Tree Structure Path
@@ -16,25 +18,55 @@ public class DeclanCiaranDijkstra {
 
 
     public static void main(String[]args)throws FileNotFoundException{
-    	readFile();
+    	
+        
+        Bag<DirectedEdge>[] adj;
+        
+        readFile();
         for(int j=0; j<Connections;j++){
-        System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
+        	System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
         }
+        //EdgeWeightedDigraph G; 
+        //    G = new EdgeWeightedDigraph(new In("point.txt"));
+       /*
+        adj = (Bag<DirectedEdge>[]) new Bag[Connections];
+        for(int i = 0;i<Connections;i++){
+        
+        adj[i] = new Bag<DirectedEdge>();}
+        int s = TSP[0][0];
+        System.out.println(adj[0]);
 
 
-
+// ----------Do algorithm
 
          LinkedList<Node> list = new LinkedList<Node>();
-
+         int count = 0;
 		        Node[] nodeArray = new Node[Connections];
 		        for(int k = 0; k < Connections; k++)
 		        {
 					nodeArray[k] = new Node(TSP[k][0],TSP[k][1],distance[k]);
-
+                                        
 					list.add(nodeArray[k]);
+                                        count++;
 				}
 
 				System.out.println(list);
+                                //System.out.println("count is "+count);
+                                //System.out.println("connections is "+Connections);
+        
+        */                 
+        //Start algorithm
+        distTo = new double[Connections];
+        edgeTo = new Node[Connections];
+        for (int v = 0; v < Connections; v++){
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        //distTo[TSP[0][0]] = 0.0;
+        distTo[TSP[0][0]-1] = 0.0;
+        System.out.print("Test "+distTo[TSP[0][0]-1]);
+        
+        
+        
 
     }
 
@@ -46,9 +78,10 @@ public class DeclanCiaranDijkstra {
         //Scanner scan = new Scanner(new File("Points.txt"));
 
         String workingDir = System.getProperty("user.dir");
-        System.out.println(workingDir);
-        //Scanner scan = new Scanner(new File(workingDir+"\\src\\ReadFile\\DensePoints.txt"));
-        Scanner scan = new Scanner(new File("DensePoints.txt"));
+        //System.out.println(workingDir);
+        Scanner scan = new Scanner(new File(workingDir+"\\src\\ReadFile\\Points.txt"));
+        //Scanner scan = new Scanner(new File("Points.txt"));
+        //Scanner scan = new Scanner(new In("Points.txt"));
         GraphType = scan.next().charAt(0);
         Connections = scan.nextInt();
         //int[] nodex = new int[Connections];
@@ -68,17 +101,44 @@ public class DeclanCiaranDijkstra {
             NodeWeight = scan.nextInt();
             distance[i] = NodeWeight;
             i++;
-        }
+
+		}
 
         System.out.println(GraphType);
         System.out.println(Connections);
-        //for(int j=0; j<Connections;j++){
-        //System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
+       // for(int j=0; j<Connections;j++){
+       // System.out.println(TSP[j][0]+" "+TSP[j][1]+" "+distance[j]);
         //return new Node();
+		//}
+		// ---------Sort arrays
+		int[] check = new int[3];
+		for(int e = 0; e < Connections; e++)
+		{
+			for(int f = 1; f < Connections; f++)
+			{
+				if(TSP[f][0] < TSP[f-1][0])
+				{
+					check[0] = TSP[f][0];
+					check[1] = TSP[f][1];
+					check[2] = distance[f];
 
-        // -----------------------It#s algorithm time
+					TSP[f][0] = TSP[f-1][0];
+					TSP[f][1] = TSP[f-1][1];
+					distance[f] = distance[f-1];
 
+					TSP[f-1][0] = check[0];
+					TSP[f-1][1] = check[1];
+					distance[f-1] = check[2];
 
-	}
+				} // end of if
+                                
+			} // end of for
+		}
+            scan.close();
+            //EdgeWeightedDigraph G; 
+            //G = new EdgeWeightedDigraph(new In("point.txt"));
+            //int s = TSP[0][0];
+            //System.out.println("G = "+G);
+	} // end of readfile
 
-}
+} // end of class
--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,21 @@
 public class Node {
 
-		private int vertex;
-		private int distance;
-		private int vertex2;
+    private int vertex;
+    private int distance;
+    private int vertex2;
 
-		Node(int a,int b,int c) {
-			vertex = a;
-			vertex2 = b;
-			distance = c;
-		}
-	}
+    public Node(int a,int b,int c) {
+        vertex = a;
+	vertex2 = b;
+	distance = c;
+    }
+    public int from(){
+        return vertex;
+    }
+    public int to(){
+        return vertex2;
+    }
+    public int weight(){
+        return distance;
+    }
+}
